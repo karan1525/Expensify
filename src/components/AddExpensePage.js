ead6f2a89e27cac5d0861c7cd5a27278c75068e8
Karan Bhargava <karan1525@yahoo.co.in>
2018-01-25 23:37:37 -0800

    Ran linter and added dynamic url linking

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,9 +1,5 @@
 import React from 'react';
 
-const AddExpensePage = () => (
-  <div>
-    This is from my add expense component
-  </div>
-);
+const AddExpensePage = () => <div>This is from my add expense component</div>;
 
 export default AddExpensePage;
--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
-const EditExpensePage = () => (
-  <div>
-    This is from my edit expense component
-  </div>
-);
+const EditExpensePage = props => {
+  return <div>Editing the expense with ID of {props.match.params.id}</div>;
+};
 
 export default EditExpensePage;
