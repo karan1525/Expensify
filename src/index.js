ba2d356db5623d2d0ee8b696d7ce13d36c5eed5f
Karan Bhargava <karan1525@yahoo.co.in>
2018-01-25 23:16:22 -0800

    Added initial routes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>This is my test</p>, document.querySelector('#root'));
+const expenseDashboard = () => <div>This is from my dashboard component</div>;
+
+const addExpense = () => <div>This is from my add expense component</div>;
+
+const editExpense = () => <div>This is from my edit expense component</div>;
+
+const helpPage = () => <div>This is from my help page component</div>;
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route exact path="/" component={expenseDashboard} />
+      <Route path="/create" component={addExpense} />
+      <Route path="/edit" component={editExpense} />
+      <Route path="/help" component={helpPage} />
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.querySelector('#root'));
