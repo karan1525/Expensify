1261ffbfd0963859610869f15555ce01645dabf7
Karan Bhargava <karan1525@yahoo.co.in>
2018-01-30 00:44:47 -0800

    Test push

--- src/index.js
+++ src/index.js
@@ -9,6 +9,22 @@ import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 import { firebase } from './firebase/firebase';
 
+const path = require('path');
+const express = require('express');
+const app = express();
+const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log('Server is up!');
+});
+
 const store = configureStore();
 
 ReactDOM.render(<p>Loading...</p>, document.querySelector('#root'));
