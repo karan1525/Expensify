ffdea6c6db09f16d66abaaa70423a1cfb2ac9dbb
Karan Bhargava <karan1525@yahoo.co.in>
2018-01-29 19:03:39 -0800

    Fixed header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,10 @@ import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { startLogout } from '../actions/auth';
 
-const Header = ({ startLogout }) => (
+export const Header = ({ startLogout }) => (
   <header>
     <h1>Expensify</h1>
-    <NavLink to="/" activeClassName="is-active" exact={true}>
+    <NavLink to="/dashboard" activeClassName="is-active">
       Dashboard
     </NavLink>
     <NavLink to="/create" activeClassName="is-active">
